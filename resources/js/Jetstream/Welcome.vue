1c7f87d09277ec2edf3a66e3df4e163ec6587b30
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-08 23:08:56 +0200

    Removed Jetstram logo

--- resources/js/Jetstream/Welcome.vue
+++ resources/js/Jetstream/Welcome.vue
@@ -1,10 +1,6 @@
 <template>
     <div>
         <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-            <div>
-                <jet-application-logo class="block h-12 w-auto" />
-            </div>
-
             <div class="mt-8 text-2xl">
                 Welcome to your Jetstream application!
             </div>
@@ -92,13 +88,3 @@
         </div>
     </div>
 </template>
-
-<script>
-    import JetApplicationLogo from './../Jetstream/ApplicationLogo'
-
-    export default {
-        components: {
-            JetApplicationLogo,
-        },
-    }
-</script>
