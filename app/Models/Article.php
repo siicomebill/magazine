cc712b9bda8e7d21459f9a9c96624b2966075be2
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-09 13:52:19 +0200

    Created article and category relations

---
+++ app/Models/Article.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
+
+    public function author()
+    {
+        return $this->belongsTo('App\Models\User', 'id', 'user_id', 'users');
+    }
+}
