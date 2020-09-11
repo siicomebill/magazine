5b36aa896df1cda75e2a947612f34f4ec496e321
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-11 12:16:41 +0200

    Basic articles display

--- resources/js/Pages/ArticlesManager.vue
+++ resources/js/Pages/ArticlesManager.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
-      <p>yo</p>
+    <p v-for="article in articles" :key="article.id">{{article.title}}</p>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    articles: Array,
+  },
+};
 </script>
-
-<style>
-
-</style>
