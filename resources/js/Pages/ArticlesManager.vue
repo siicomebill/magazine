73175874175554ed7d29c888d5c46e9e1a3847be
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-11 14:51:22 +0200

    Display articles with Reader component

--- resources/js/Pages/ArticlesManager.vue
+++ resources/js/Pages/ArticlesManager.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    <p v-for="article in articles" :key="article.id">{{article.title}}</p>
+    <div v-for="article in articles" :key="article.id">
+      <p>{{article.title}}</p>
+      <p>{{article.snippet}}</p>
+      <Reader v-model="article.content" />
+    </div>
   </div>
 </template>
 
 <script>
+import { Reader } from 'vue-publisher'
 export default {
+  components: {
+    Reader
+  },
   props: {
     articles: Array,
   },
