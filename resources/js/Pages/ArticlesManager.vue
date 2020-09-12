5ea8b04a9011b1f3fc729c45b140a4f23deb4491
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-12 17:33:10 +0200

    Added placeholder buttons

--- resources/js/Pages/ArticlesManager.vue
+++ resources/js/Pages/ArticlesManager.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <div class="container mx-auto">
-      <Card v-bind="article" v-for="article in articles" :key="article.id" />
+      <Card v-bind="article" v-for="article in articles" :key="article.id">
+        <template #footer>
+          <div>
+            <div class="justify-end flex">
+              <button class="mx-1 hover:shadow-xl bg-gray-100 px-4 py-2 rounded-lg transition-shadow duration-200">Edit</button>
+              <button class="mx-1 hover:shadow-xl bg-red-600 text-white px-4 py-2 rounded-lg transition-shadow duration-200">Delete</button>
+            </div>
+          </div>
+        </template>
+      </Card>
     </div>
   </div>
 </template>
