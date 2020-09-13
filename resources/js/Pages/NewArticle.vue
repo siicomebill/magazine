13dbe8f173a29a2c67f5d81a5d984b573689796a
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-13 15:43:09 +0200

    Display form validation errors

--- resources/js/Pages/NewArticle.vue
+++ resources/js/Pages/NewArticle.vue
@@ -2,23 +2,41 @@
   <div>
     <div class="class container mx-auto my-4 rounded-lg p-4">
       <form @submit.prevent="publish">
-        <input
-          type="text"
-          v-model="article.title"
-          placeholder="Title"
-          class="appearance-none bg-white shadow border-none w-full leading-tight p-4 mb-4 rounded-lg text-3xl font-bold text-gray-700"
-        />
+        <div class="mb-4">
+          <input
+            type="text"
+            v-model="article.title"
+            placeholder="Title"
+            class="appearance-none bg-white shadow w-full leading-tight p-4 rounded-lg text-3xl font-bold text-gray-700"
+            :class="{ 'border border-red-500': error('title') }"
+          />
+          <span class="text-red-500 text-sm italic" v-if="error('title')">{{error('title')}}</span>
+        </div>
 
-        <textarea
-          type="text"
-          v-model="article.snippet"
-          placeholder="Snippet"
-          class="appearance-none bg-white shadow border-none w-full leading-tight p-4 my-4 rounded-lg text-xl text-gray-700"
-        />
+        <div class="my-4">
+          <textarea
+            type="text"
+            v-model="article.snippet"
+            placeholder="Snippet"
+            class="appearance-none bg-white shadow w-full leading-tight p-4 rounded-lg text-xl text-gray-700"
+            :class="{ 'border border-red-500': error('snippet') }"
+          />
+          <span class="text-red-500 text-sm italic" v-if="error('snippet')">{{error('snippet')}}</span>
+        </div>
 
-        <Publisher class="shadow rounded-lg p-2 bg-white" v-model="article.content" />
+        <div class="my-4">
+          <Publisher
+            v-model="article.content"
+            class="shadow rounded-lg p-2 bg-white"
+            :class="{ 'border border-red-500': error('snippet') }"
+          />
+          <span class="text-red-500 text-sm italic" v-if="error('content')">{{error('content')}}</span>
+        </div>
 
-        <button type="submit" class="text-lg bg-blue-500 text-white font-bold rounded-lg mt-4 w-full p-4">Publish</button>
+        <button
+          type="submit"
+          class="text-lg bg-blue-500 text-white font-bold rounded-lg mt-4 w-full p-4"
+        >Publish</button>
       </form>
     </div>
   </div>
@@ -32,9 +50,12 @@ export default {
     Publisher,
   },
   methods: {
-    publish() {
-      console.log(this.article);
+    async publish() {
+      let response = await this.$inertia.post(this.publishTo, this.article);
     },
+  },
+  props: {
+    publishTo: String,
   },
   data() {
     return {
