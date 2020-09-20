86e6f517fef24d3dd6c66ad73e9d1bcbb4262beb
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-20 16:36:13 +0200

    Fixed rounded corners mismatch

--- resources/js/Components/Card.vue
+++ resources/js/Components/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full lg:flex bg-white rounded-lg my-4 shadow-xl">
-    <div v-if="image" class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" :style="{ 'background-image': 'url(' + image + ')' }" title="Thumbnail"></div>
+    <div v-if="image" class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t-lg lg:rounded-t-none lg:rounded-l-lg text-center overflow-hidden" :style="{ 'background-image': 'url(' + image + ')' }" title="Thumbnail"></div>
     <div class="flex flex-col p-4 w-full justify-between leading-normal">
       <div class="mb-8">
         <p class="font-bold text-2xl text-primary mb-2">{{title}}</p>
