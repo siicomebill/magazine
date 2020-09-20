a8e71364213331586eddf3dd47a2fe608aae2687
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-20 21:58:32 +0200

    Re-styled banner

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <div class="py-20 bg-black text-white -mb-10 pb-30">
+    <div class="py-20 bg-black banner text-white -mb-10 pb-30 rounded-br-full md:px-10 lg:px-0">
       <div class="container mx-auto">
-        <h1 class="text-6xl">Bill</h1>
-        <p class="text-sm">This is just a demo, the content and the graphics will be worked on in the end.</p>
+        <h1 class="text-6xl font-bold">Sii Come Bill</h1>
+        <p
+          class="text-md"
+        >This is just a demo, the content and the graphics will be worked on in the end. Here is where the slogan will be</p>
       </div>
     </div>
     <div class="container mx-auto grid grid-cols-3 gap-4">
@@ -26,3 +28,10 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.banner {
+  background-color: #000000;
+  background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.16'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+}
+</style>
