570c8b126c16d772e0bdb97bc6af9334fda3481e
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-20 22:25:49 +0200

    Added link to categories page

--- resources/js/Pages/Categories.vue
+++ resources/js/Pages/Categories.vue
@@ -3,7 +3,7 @@
     <div class="container mx-auto py-20">
       <h1 class="text-6xl">Categorie</h1>
     </div>
-    <a v-for="category in categories" :key="category.id" class="block py-10 bg-black text-white" :href="$route('category.articles', category.id)">
+    <a v-for="category in categories" :key="category.id" class="block py-10 bg-black text-white" :href="$route('categories.articles', category.id)">
       <div class="container mx-auto">
         <p class="text-4xl">{{category.name}}</p>
       </div>
