5d3b9b565682b6ebdf8c25083841e5ac0009253d
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-20 18:42:37 +0200

    Added button for new article

--- resources/js/Pages/ArticlesManager.vue
+++ resources/js/Pages/ArticlesManager.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <div class="container mx-auto">
+    <div class="container mx-auto my-5">
+      <a
+        :href="$route('articles.write')"
+        class="w-full block hover:shadow-xl bg-white border-gray-200 text-center px-6 py-3 rounded-lg transition-shadow duration-200"
+      >Nuovo</a>
+
       <Card v-bind="article" v-for="article in articles" :key="article.id">
         <template #footer>
           <div>
             <div class="justify-end flex">
-              <a :href="article.links.edit" class="mx-1 hover:shadow-xl bg-gray-100 px-4 py-2 rounded-lg transition-shadow duration-200">Edit</a>
-              <a :href="article.links.delete" class="mx-1 hover:shadow-xl bg-red-600 text-white px-4 py-2 rounded-lg transition-shadow duration-200">Delete</a>
+              <a
+                :href="article.links.edit"
+                class="mx-1 hover:shadow-xl bg-gray-100 px-4 py-2 rounded-lg transition-shadow duration-200"
+              >Edit</a>
+              <a
+                :href="article.links.delete"
+                class="mx-1 hover:shadow-xl bg-red-600 text-white px-4 py-2 rounded-lg transition-shadow duration-200"
+              >Delete</a>
             </div>
           </div>
         </template>
@@ -17,7 +28,7 @@
 
 <script>
 import Card from "~/Card";
-import Layout from '@/Layouts/AppLayout'
+import Layout from "@/Layouts/AppLayout";
 
 export default {
   layout: Layout,
