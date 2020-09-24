890ca4e944930df936ba7ebf9c76d9c1de7bb810
Mattia Sinisi <mattia@snisni.it>
2020-09-24 15:12:46 +0200

    Include sponsors in homepage

--- app/Http/Controllers/PublicPagesController.php
+++ app/Http/Controllers/PublicPagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use App\Models\Sponsor;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -11,7 +12,8 @@ class PublicPagesController extends Controller
     public function index(Request $request)
     {
         return Inertia::render('Home', [
-            "articles" => Article::with(['author'])->get()
+            "articles" => Article::with(['author'])->get(),
+            "sponsors" => Sponsor::all(),
         ]);
     }
 }
