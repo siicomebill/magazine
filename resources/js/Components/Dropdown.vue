5e28c8a07ca9932962445d8c08235a9fcb8f318f
Mattia Sinisi <mattia@snisni.it>
2020-09-24 12:32:10 +0200

    Removed excessive styling from navbar

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -4,7 +4,7 @@
       <div class="-mr-2 flex items-center">
         <button
           @click="showDropdown = !showDropdown"
-          class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+          class="inline-flex items-center justify-center p-2 rounded-md text-gray-500 focus:outline-none"
         >
           <svg
             class="h-6 w-6"
