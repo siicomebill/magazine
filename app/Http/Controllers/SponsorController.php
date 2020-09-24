c7a9db5cb914f515a7423904c59db58b96cff60c
Mattia Sinisi <mattia@snisni.it>
2020-09-24 15:38:18 +0200

    Added sponsor managing page

--- app/Http/Controllers/SponsorController.php
+++ app/Http/Controllers/SponsorController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Sponsor;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class SponsorController extends Controller
 {
-    //
+    public function index()
+    {
+        //TODO Add links like in ArticleController
+        return Inertia::render('SponsorsManager', [
+            "sponsors" => Sponsor::all()
+        ]);
+    }
 }
---
+++ resources/js/Pages/SponsorsManager.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="container mx-auto my-5">
+      <a
+        :href="$route('articles.write')"
+        class="w-full block hover:shadow-xl bg-white border-gray-200 text-center px-6 py-3 rounded-lg transition-shadow duration-200"
+      >Nuovo</a>
+
+      <Card v-bind="article" v-for="article in sponsors" :key="article.id">
+        <template #footer>
+          <div>
+            <div class="justify-end flex">
+              <a
+                :href="article.links.edit"
+                class="mx-1 hover:shadow-xl bg-gray-100 px-4 py-2 rounded-lg transition-shadow duration-200"
+              >Edit</a>
+              <a
+                :href="article.links.delete"
+                class="mx-1 hover:shadow-xl bg-red-600 text-white px-4 py-2 rounded-lg transition-shadow duration-200"
+              >Delete</a>
+            </div>
+          </div>
+        </template>
+      </Card>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "~/Card";
+import Layout from "@/Layouts/AppLayout";
+
+export default {
+  layout: Layout,
+  components: {
+    Card,
+  },
+  props: {
+    sponsors: Array,
+  },
+};
+</script>
