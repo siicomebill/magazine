a333e8487b55f70a581f001ede639ee929126fea
Mattia Sinisi <mattia@snisni.it>
2020-09-24 14:41:35 +0200

    Revised card look + Implemented Thumbnail component

--- resources/js/Components/Card.vue
+++ resources/js/Components/Card.vue
@@ -1,10 +1,17 @@
 <template>
-  <a class="block w-full lg:flex bg-white rounded-lg my-4 shadow-xl" :href="href">
-    <div v-if="image" class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t-lg lg:rounded-t-none lg:rounded-l-lg text-center overflow-hidden" :style="{ 'background-image': 'url(' + image + ')' }" title="Thumbnail"></div>
-    <div class="flex flex-col p-4 w-full justify-between leading-normal">
+  <a
+    class="block w-full bg-white rounded-lg my-4 shadow-xl"
+    :href="href"
+  >
+    <Thumbnail
+      :src="image"
+      aspectRatio="square"
+      class="w-auto rounded-t-lg text-center overflow-hidden"
+    />
+    <div class="flex flex-col p-4 w-50 justify-between leading-normal">
       <div class="mb-8">
-        <p class="font-bold text-2xl text-primary mb-2">{{title}}</p>
-        <p class="text-gray-700 text-base">{{snippet}}</p>
+        <p class="font-bold text-2xl text-primary mb-2">{{ title }}</p>
+        <p class="text-gray-700 text-base">{{ snippet }}</p>
         <div class="my-3">
           <img
             class="w-10 h-10 rounded-full mr-4"
@@ -13,7 +20,7 @@
             :src="author.profile_photo_path"
           />
           <div class="text-sm">
-            <p class="text-gray-500 leading-none">{{author.name}}</p>
+            <p class="text-gray-500 leading-none">{{ author.name }}</p>
           </div>
         </div>
 
@@ -30,7 +37,12 @@
 </template>
 
 <script>
+import Thumbnail from "~/Thumbnail";
+
 export default {
+  components: {
+    Thumbnail,
+  },
   props: {
     title: {
       type: String,
