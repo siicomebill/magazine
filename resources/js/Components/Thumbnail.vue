d6501b5e0f63fd52f92c3aa43e4576bb3bae13d8
Mattia Sinisi <mattia@snisni.it>
2020-09-24 14:28:51 +0200

    Created Thumbnail component

---
+++ resources/js/Components/Thumbnail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative">
+    <div :class="`aspect-ratio-${aspectRatio}`"></div>
+    <img :src="src" class="absolute left-0 top-0 bottom-0 right-0 w-full h-full object-cover" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    aspectRatio: {
+      type: String,
+      default: "16:9"
+    },
+    src: String,
+    alt: String,
+  },
+};
+</script>
