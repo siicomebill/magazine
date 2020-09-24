958dac1e306d4751c00a32b0f34a8e172d934611
Mattia Sinisi <mattia@snisni.it>
2020-09-24 11:11:15 +0200

    Re-organized article header

--- resources/js/Pages/Article.vue
+++ resources/js/Pages/Article.vue
@@ -9,19 +9,21 @@
 
         <h1 class="text-6xl align-middle">{{article.title}}</h1>
 
-        <div>{{article.snippet}}</div>
-      </div>
-    </div>
+        <div class="my-4 rounded-lg">
+          <img
+            class="w-10 h-10 rounded-full mr-4"
+            v-if="article.author.profile_photo_path"
+            :alt="article.author.name[0]"
+            :src="article.author.profile_photo_path"
+          />
+          <div class="text-xl">
+            <p class="leading-none">{{article.author.name}}</p>
+          </div>
+        </div>
 
-    <div class="container mx-auto my-4 p-3 rounded-lg">
-      <img
-        class="w-10 h-10 rounded-full mr-4"
-        v-if="article.author.profile_photo_path"
-        :alt="article.author.name[0]"
-        :src="article.author.profile_photo_path"
-      />
-      <div class="text-sm">
-        <p class="text-gray-500 leading-none">{{article.author.name}}</p>
+        <hr class="my-6 w-11/12 mx-auto"/>
+
+        <div>{{article.snippet}}</div>
       </div>
     </div>
 
