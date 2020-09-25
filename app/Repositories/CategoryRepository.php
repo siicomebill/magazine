a2523603ba50d60fea5ab754e2639fe8b681674f
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-25 22:30:47 +0200

    Re-declared important definition for categories

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -7,4 +7,9 @@ use App\Models\Category;
 class CategoryRepository extends ResourceRepository
 {
     protected $model = Category::class;
+
+    public function important()
+    {
+        return $this->model::limit(10);
+    }
 }
