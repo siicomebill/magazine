0409b88b5ba4f763f0a03e40f5712931946f24c4
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-25 22:25:22 +0200

    Use repositories

--- app/Http/Controllers/PublicPagesController.php
+++ app/Http/Controllers/PublicPagesController.php
@@ -4,16 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use App\Models\Sponsor;
+use App\Repositories\ArticleRepository;
+use App\Repositories\SponsorRepository;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class PublicPagesController extends Controller
 {
-    public function index(Request $request)
+    public function index(ArticleRepository $article, SponsorRepository $sponsor)
     {
         return Inertia::render('Home', [
-            "articles" => Article::all(),
-            "sponsors" => Sponsor::all(),
+            "articles" => $article->get(),
+            "sponsors" => $sponsor->get(),
         ]);
     }
 }
