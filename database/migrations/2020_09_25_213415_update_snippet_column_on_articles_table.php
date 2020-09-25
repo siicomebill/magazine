6c151f1f3e32576dfefcc81dc4cada0d045d70af
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-25 23:36:09 +0200

    Updated snippet column to be of type text

---
+++ database/migrations/2020_09_25_213415_update_snippet_column_on_articles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateSnippetColumnOnArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->text('snippet')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
