8407627e28186460dfaab87c423c35eb55216438
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-26 01:00:31 +0200

    Updated interface

--- app/Interfaces/Repositories/ResourceRepositoryInterface.php
+++ app/Interfaces/Repositories/ResourceRepositoryInterface.php
@@ -4,6 +4,18 @@ namespace App\Interfaces\Repositories;
 
 interface ResourceRepositoryInterface
 {
+    public function asModel();
+
+    public function get();
+
+    public function important();
+
+    public function others();
+
+    public function latest(int $limit = 0);
+
+    public function find($id);
+
     public function store($request);
 
     public function delete($id);
