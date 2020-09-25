922a5adbc6d2ee2cdb57fe80be4628a46b1d79a4
Mattia Sinisi <mattia@snisni.it>
2020-09-25 17:06:30 +0200

    Construct using correct repository

--- app/Http/Controllers/SponsorController.php
+++ app/Http/Controllers/SponsorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Base\ResourceController;
 use App\Models\Sponsor;
+use App\Repositories\SponsorRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Inertia\Inertia;
@@ -16,4 +17,9 @@ class SponsorController extends ResourceController
         "managerPage" => "SponsorsManager",
         "newItemPage" => "NewSponsor"
     ];
+
+    public function __construct(SponsorRepository $sponsor)
+    {
+        parent::__construct($sponsor);
+    }
 }
