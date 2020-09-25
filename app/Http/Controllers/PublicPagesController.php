94a70ce6c70183c2209386d5ed36e0c5997ccbf0
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-25 22:31:01 +0200

    Get categories

--- app/Http/Controllers/PublicPagesController.php
+++ app/Http/Controllers/PublicPagesController.php
@@ -5,17 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use App\Models\Sponsor;
 use App\Repositories\ArticleRepository;
+use App\Repositories\CategoryRepository;
 use App\Repositories\SponsorRepository;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class PublicPagesController extends Controller
 {
-    public function index(ArticleRepository $article, SponsorRepository $sponsor)
+    public function index(ArticleRepository $article, SponsorRepository $sponsor, CategoryRepository $category)
     {
         return Inertia::render('Home', [
             "articles" => $article->get(),
             "sponsors" => $sponsor->get(),
+            "categories" => $category->important()->get()
         ]);
     }
 }
