a33fcdb1c5613db8787ef7c8a41888e83adfc4d4
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-26 00:22:34 +0200

    Improved responsive design

--- resources/js/Components/MiniCard.vue
+++ resources/js/Components/MiniCard.vue
@@ -7,9 +7,9 @@
       :src="image"
       :alt="title"
       aspectRatio="square"
-      class="block w-1/4 rounded-lg text-center overflow-hidden"
+      class="block w-1/2 rounded-lg text-center overflow-hidden"
     />
-    <div class="flex flex-col p-4 w-50 justify-between leading-normal">
+    <div class="flex flex-col p-4 w-1/2 justify-between leading-normal">
       <div class="mb-8">
         <p class="font-bold text-2xl text-primary mb-2">{{ title }}</p>
         <div class="my-3">
