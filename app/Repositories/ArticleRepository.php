24696745eb2a87a62842e96b8a9c6ab4f74698cb
Mattia Sinisi <mattia@snisni.it>
2020-09-25 16:40:29 +0200

    Correctly import Article model

--- app/Repositories/ArticleRepository.php
+++ app/Repositories/ArticleRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Interfaces\Repositories\ArticleRepositoryInterface;
+use App\Models\Article;
 
 class ArticleRepository extends ResourceRepository implements ArticleRepositoryInterface
 {
