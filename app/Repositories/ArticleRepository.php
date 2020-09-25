26445e6926ce2f188e27103975a319da68916cb7
Mattia Sinisi <mattia@snisni.it>
2020-09-25 16:36:28 +0200

    Implemented store function

--- app/Repositories/ArticleRepository.php
+++ app/Repositories/ArticleRepository.php
@@ -2,7 +2,29 @@
 
 namespace App\Repositories;
 
-class ArticleRepository extends ResourceRepository
+use App\Interfaces\Repositories\ArticleRepositoryInterface;
+
+class ArticleRepository extends ResourceRepository implements ArticleRepositoryInterface
 {
     protected $model = Article::class;
+
+    public function store($request, $user = null)
+    {
+        if(!$user) return false;
+
+        if ($request->id) {
+            $article = $user->articles()->find($request->id);
+
+            if ($article) {
+                $article->update($request->all());
+                return true;
+            } else {
+                //TODO Populate error bag
+                return false;
+            }
+        } else {
+            $user->articles()->create($request->all());
+            return true;
+        }
+    }
 }
