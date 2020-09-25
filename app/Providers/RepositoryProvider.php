9957afa1b4b97e7a6cdf37ab751f9feff7d77699
Mattia Sinisi <mattia@snisni.it>
2020-09-25 17:05:41 +0200

    Added sponsor repository to provider

--- app/Providers/RepositoryProvider.php
+++ app/Providers/RepositoryProvider.php
@@ -7,6 +7,7 @@ use App\Interfaces\Repositories\ArticleRepositoryInterface;
 use App\Interfaces\Repositories\ResourceRepositoryInterface;
 use App\Repositories\ArticleRepository;
 use App\Repositories\ResourceRepository;
+use App\Repositories\SponsorRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryProvider extends ServiceProvider
@@ -20,6 +21,7 @@ class RepositoryProvider extends ServiceProvider
     {
         $this->app->bind(ResourceRepositoryInterface::class, ResourceRepository::class);
         $this->app->bind(ArticleRepositoryInterface::class, ArticleRepository::class);
+        $this->app->bind(ResourceRepositoryInterface::class, SponsorRepository::class);
     }
 
     /**
