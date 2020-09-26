a5f62c3857bf54ed7160af5bb571b2505d285db5
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-26 22:40:42 +0200

    Created a mini-sponsor card

---
+++ resources/js/Components/MiniSponsorCard.vue
@@ -0,0 +1,11 @@
+<script>
+import SponsorCard from '~/SponsorCard';
+import Card from '~/MiniCard';
+
+export default {
+    extends: SponsorCard,
+    components: {
+        Card,
+    },
+}
+</script>
