63d383b9a89bc2fc8ccab98a8865afac4cefa08e
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-26 21:45:56 +0200

    Mapped sponsor object to card component

--- resources/js/Components/SponsorCard.vue
+++ resources/js/Components/SponsorCard.vue
@@ -1,22 +1,34 @@
 <template>
-  <Card>
+  <Card :title="name" :snippet="snippet" :image="image" :href="link">
     <template #footer>
-        hey
+        <span class="button">{{cta}}</span>
     </template>
   </Card>
 </template>
 
 <script>
-import Card from '~/MiniCard';
+import Card from "~/Card";
 
 export default {
-    extends: Card,
-    components: {
-        Card,
+  extends: Card,
+  components: {
+    Card,
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
     },
-}
+    snippet: String,
+    cta: {
+      type: String,
+      default: "Visita",
+    },
+    image: String,
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
