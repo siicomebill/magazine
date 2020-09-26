e27ecb128c79ce18283217c1a003ea95fb975209
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-26 21:40:30 +0200

    Created basic sponsor card

---
+++ resources/js/Components/SponsorCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <Card>
+    <template #footer>
+        hey
+    </template>
+  </Card>
+</template>
+
+<script>
+import Card from '~/MiniCard';
+
+export default {
+    extends: Card,
+    components: {
+        Card,
+    },
+}
+</script>
+
+<style>
+
+</style>
