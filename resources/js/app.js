5473dc10f4c3e5a64196907f7e3f6b1ff5a941b7
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-26 23:57:07 +0200

    Using scroll spy

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,12 +7,14 @@ import { InertiaForm } from "laravel-jetstream";
 import PortalVue from "portal-vue";
 import route from "ziggy-js";
 import LazyLoad from 'vue-lazyload';
+import ScrollSpy from 'vue2-scrollspy';
 
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
 Vue.use(PortalVue);
 Vue.prototype.$route = (...args) => route(...args).url();
 Vue.use(LazyLoad);
+Vue.use(ScrollSpy);
 
 Vue.mixin({
     methods: {
