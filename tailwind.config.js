e19b817db34730fc5927d3e1868b5933a1349a51
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-27 12:45:00 +0200

    Switched to layers mode for production purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,39 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    purge: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php', './resources/js/**/*.vue'],
+	purge: {
+		layers: ["utilities"],
+		content: [
+			"./storage/framework/views/*.php",
+			"./resources/views/**/*.blade.php",
+			"./resources/js/**/*.vue"
+		]
+	},
 
-    future: {
-        removeDeprecatedGapUtilities: true,
-        purgeLayersByDefault: true,
-    },
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true
+	},
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
-        aspectRatio: {
-            'none': 0,
-            'square': [1, 1],
-            '16:9': [16, 9],
-            '4:3': [4, 3],
-            '21:9': [21, 9],
-        },
-    },
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Nunito", ...defaultTheme.fontFamily.sans]
+			}
+		},
+		aspectRatio: {
+			none: 0,
+			square: [1, 1],
+			"16:9": [16, 9],
+			"4:3": [4, 3],
+			"21:9": [21, 9]
+		}
+	},
 
-    variants: {
-        opacity: ['responsive', 'hover', 'focus', 'disabled'],
-        aspectRatio: ['responsive'],
-    },
+	variants: {
+		opacity: ["responsive", "hover", "focus", "disabled"],
+		aspectRatio: ["responsive"]
+	},
 
-    plugins: [require('@tailwindcss/ui'), require('tailwindcss-aspect-ratio')],
+	plugins: [require("@tailwindcss/ui"), require("tailwindcss-aspect-ratio")]
 };
