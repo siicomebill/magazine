6a996ab7a3fe2c36232d998a816067364aa44624
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-27 14:50:40 +0200

    Using public layout as a component

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,85 +1,100 @@
 <template>
-  <div class="content-background">
-    <div
-      class="py-20 bg-black banner text-white text-center -mb-20 pb-40 lg:rounded-br-full md:px-10 lg:px-0"
-    >
-      <div class="container mx-auto px-4 md:px-0">
-        <h1 class="md:text-6xl text-4xl font-bold">Sii Come Bill</h1>
-        <h2>Il Magazine Intelligente</h2>
+  <Layout>
+    <template #menu>
+      hey
+    </template>
+    <div class="content-background">
+      <div
+        class="py-20 bg-black banner text-white text-center -mb-20 pb-40 lg:rounded-br-full md:px-10 lg:px-0"
+      >
+        <div class="container mx-auto px-4 md:px-0">
+          <h1 class="md:text-6xl text-4xl font-bold">Sii Come Bill</h1>
+          <h2>Il Magazine Intelligente</h2>
+        </div>
       </div>
-    </div>
-    <div class="mx-auto lg:w-8/12 lg:grid grid-cols-6 gap-2">
-      <div class="col-span-5 z-10" v-scroll-spy>
-        <div v-for="(category, i) in categories" :key="category.id">
-          <div class="bg-white rounded-lg shadow-lg p-6 mb-5">
-            <a
-              :href="$route('categories.articles', category.id)"
-              class="block text-3xl text-center lg:text-left lg:text-6xl capitalize font-bold"
-            >
-              {{ category.name }}
-            </a>
-            <div>
-              <div class="block lg:grid grid-cols-2 grid-flow-cols gap-4">
-                <div>
-                  <Card
-                    v-bind="category.articles[0]"
-                    :href="$route('articles.read', category.articles[0].id)"
-                  />
+      <div class="mx-auto lg:w-8/12 lg:grid grid-cols-6 gap-2">
+        <div class="col-span-5 z-10" v-scroll-spy>
+          <div v-for="(category, i) in categories" :key="category.id">
+            <div class="bg-white rounded-lg shadow-lg p-6 mb-5">
+              <a
+                :href="$route('categories.articles', category.id)"
+                class="block text-3xl text-center lg:text-left lg:text-6xl capitalize font-bold"
+              >
+                {{ category.name }}
+              </a>
+              <div>
+                <div class="block lg:grid grid-cols-2 grid-flow-cols gap-4">
+                  <div>
+                    <Card
+                      v-bind="category.articles[0]"
+                      :href="$route('articles.read', category.articles[0].id)"
+                    />
+                  </div>
+
+                  <div>
+                    <MiniCard
+                      v-bind="article"
+                      v-for="article in category.articles.slice(1, 4)"
+                      :key="article.id"
+                      :href="$route('articles.read', article.id)"
+                      class="h-auto bg-transparent"
+                    />
+                  </div>
                 </div>
 
-                <div>
+                <div class="lg:grid grid-cols-2 gap-2">
                   <MiniCard
                     v-bind="article"
-                    v-for="article in category.articles.slice(1, 4)"
+                    v-for="article in category.articles.slice(5, 7)"
                     :key="article.id"
                     :href="$route('articles.read', article.id)"
-                    class="h-auto bg-transparent"
                   />
                 </div>
               </div>
 
-              <div class="lg:grid grid-cols-2 gap-2">
-                <MiniCard
-                  v-bind="article"
-                  v-for="article in category.articles.slice(5, 7)"
-                  :key="article.id"
-                  :href="$route('articles.read', article.id)"
-                />
-              </div>
+              <a
+                class="block w-full border border-1 border-black rounded-full p-3 text-center bg-white"
+                :href="$route('categories.articles', category.id)"
+              >
+                Vedi tutti
+              </a>
             </div>
 
-            <a
-              class="block w-full border border-1 border-black rounded-full p-3 text-center bg-white"
-              :href="$route('categories.articles', category.id)"
-            >
-              Vedi tutti
-            </a>
+            <MiniSponsorCard
+              v-if="sponsors[i]"
+              v-bind="sponsors[i]"
+              class="lg:w-11/12"
+            />
           </div>
+        </div>
 
-          <MiniSponsorCard v-if="sponsors[i]" v-bind="sponsors[i]" class="lg:w-11/12" />
-          
+        <div
+          class="col-span-1 relative lg:block hidden"
+          v-if="categories.length"
+        >
+          <ul
+            class="bg-white rounded-lg -ml-32 pl-32 p-4 fixed shadow-lg z-0"
+            v-scroll-spy-active="{ class: 'font-bold' }"
+            v-scroll-spy-link
+          >
+            <li v-for="category in categories" :key="category.id">
+              <a class="cursor-pointer">{{ category.name }}</a>
+            </li>
+          </ul>
         </div>
       </div>
 
-      <div class="col-span-1 relative lg:block hidden" v-if="categories.length">
-        <ul class="bg-white rounded-lg -ml-32 pl-32 p-4 fixed shadow-lg z-0" v-scroll-spy-active="{class: 'font-bold'}" v-scroll-spy-link>
-          <li v-for="category in categories" :key="category.id">
-            <a class="cursor-pointer">{{category.name}}</a>
-          </li>
-        </ul>
+      <div class="container mx-auto" v-if="articles.length">
+        <p class="text-center text-4xl text-gray-500 my-4">Esplora</p>
+        <MiniCard
+          v-bind="article"
+          v-for="article in articles"
+          :key="article.id"
+          :href="$route('articles.read', article.id)"
+        />
       </div>
     </div>
-
-    <div class="container mx-auto" v-if="articles.length">
-      <p class="text-center text-4xl text-gray-500 my-4">Esplora</p>
-      <MiniCard
-        v-bind="article"
-        v-for="article in articles"
-        :key="article.id"
-        :href="$route('articles.read', article.id)"
-      />
-    </div>
-  </div>
+  </Layout>
 </template>
 
 <script>
@@ -90,8 +105,8 @@ import MiniSponsorCard from "~/MiniSponsorCard";
 import Layout from "@/Layouts/PublicLayout";
 
 export default {
-  layout: Layout,
   components: {
+    Layout,
     Card,
     MiniCard,
     SponsorCard,
