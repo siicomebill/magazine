75026bf8a17b0e3e16422af6bf001288df978fa7
sinisimattia <mattia.sinisi@virgilio.it>
2020-09-27 14:40:30 +0200

    Re-designed sponsor card

--- resources/js/Components/SponsorCard.vue
+++ resources/js/Components/SponsorCard.vue
@@ -1,23 +1,21 @@
 <template>
-  <Card
-    :title="name"
-    :image="image"
-    :href="link"
-    target="_blank"
-  >
-    {{snippet}}
-    <template #footer>
-      <div class="grid">
-        <span
-          class="bg-blue-500 text-white text-center font-bold rounded-full py-2 lg:pr-4 lg:pl-10 lg:-mr-12 lg:justify-self-end"
-        >
-          {{ cta }}
-        </span>
-      </div>
+  <div class="mb-4">
+    <Card :title="name" :image="image" :href="link" target="_blank" class="mb-1">
+      <span class="block">{{ snippet }}</span>
 
-      <span class="text-sm text-gray-500">Sponsor</span>
-    </template>
-  </Card>
+      <template #footer>
+        <div class="grid">
+          <span
+            class="bg-blue-500 text-white text-center font-bold rounded-full py-2 lg:pr-4 lg:pl-10 lg:-mr-12 lg:justify-self-end"
+          >
+            {{ cta }}
+          </span>
+        </div>
+      </template>
+    </Card>
+
+    <span class="text-sm text-gray-500">Sponsor</span>
+  </div>
 </template>
 
 <script>
