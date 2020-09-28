49e32a3859ab4817c033d3e6cbe5cc74bb34f81f
Mattia Sinisi <mattia@snisni.it>
2020-09-28 10:43:22 +0200

    Added compression plugin

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require('laravel-mix');
 require('laravel-mix-alias');
+const CompressionPlugin = require('compression-webpack-plugin')
 
 /*
  |--------------------------------------------------------------------------
@@ -25,4 +26,10 @@ mix
 mix.alias({
     '@': '/resources/js',
     '~': '/resources/js/Components',
+})
+
+mix.webpackConfig({
+    plugins: [
+        new CompressionPlugin(),
+    ],
 })
