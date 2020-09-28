1b98170b28de76866d86fd64e5aeb2acaca08583
Mattia Sinisi <mattia@snisni.it>
2020-09-28 14:06:56 +0200

    Navbar content inside container

--- resources/js/Layouts/PublicLayout.vue
+++ resources/js/Layouts/PublicLayout.vue
@@ -1,45 +1,47 @@
 <template>
   <div>
     <nav
-      class="sticky top-0 z-30 lg:bg-white lg:text-black bg-black text-white shadow-lg flex items-center justify-between flex flex-wrap lg:px-6 lg:py-1 px-4"
+      class="sticky top-0 z-30 lg:bg-white lg:text-black bg-black text-white shadow-lg lg:px-6 lg:py-1 px-4"
     >
-      <div class="flex items-center flex-shrink-0 mr-6">
-        <a href="/" class="font-semibold text-xl tracking-tight">BILL</a>
-      </div>
+      <div class="container mx-auto flex items-center justify-between flex flex-wrap">
+        <div class="flex items-center flex-shrink-0 mr-6">
+          <a href="/" class="font-semibold text-xl tracking-tight">BILL</a>
+        </div>
 
-      <Dropdown class="text-black flex-grow">
-        <div class="text-sm lg:flex-grow lg:mb-0 mb-4">
-          <slot name="menu">
+        <Dropdown class="text-black flex-grow">
+          <div class="text-sm lg:flex-grow lg:mb-0 mb-4">
+            <slot name="menu">
+              <a
+                :href="$route('categories')"
+                class="block lg:inline-block lg:mt-0 mx-4"
+              >
+                Categorie
+              </a>
+            </slot>
+          </div>
+
+          <div v-if="$page.auth">
             <a
-              :href="$route('categories')"
-              class="block lg:inline-block lg:mt-0 mx-4"
+              :href="$route('dashboard')"
+              class="inline-block text-sm px-4 py-2 leading-none border rounded border-black hover:bg-black hover:text-white hover:border-transparent lg:mt-0"
+              >Dashboard</a
             >
-              Categorie
-            </a>
-          </slot>
-        </div>
+          </div>
 
-        <div v-if="$page.auth">
-          <a
-            :href="$route('dashboard')"
-            class="inline-block text-sm px-4 py-2 leading-none border rounded border-black hover:bg-black hover:text-white hover:border-transparent lg:mt-0"
-            >Dashboard</a
-          >
-        </div>
-
-        <div v-else>
-          <a
-            :href="$route('login')"
-            class="inline-block text-sm px-4 py-2 leading-none border rounded border-black hover:bg-black hover:text-white hover:border-transparent lg:mt-0"
-            >Login</a
-          >
-          <a
-            :href="$route('register')"
-            class="inline-block text-sm px-4 py-2 leading-none lg:mt-0"
-            >Registrati</a
-          >
-        </div>
-      </Dropdown>
+          <div v-else>
+            <a
+              :href="$route('login')"
+              class="inline-block text-sm px-4 py-2 leading-none border rounded border-black hover:bg-black hover:text-white hover:border-transparent lg:mt-0"
+              >Login</a
+            >
+            <a
+              :href="$route('register')"
+              class="inline-block text-sm px-4 py-2 leading-none lg:mt-0"
+              >Registrati</a
+            >
+          </div>
+        </Dropdown>
+      </div>
     </nav>
 
     <main>
