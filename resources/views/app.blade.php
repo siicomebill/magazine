3bff99ff423a4b6c8d0345fb32efb6ecd3558c66
Mattia Sinisi <mattia@snisni.it>
2020-09-28 11:50:05 +0200

    Switch to minified files in production

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,11 +14,19 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
 
         <!-- Styles -->
+        @if (env('APP_ENV') == 'production')
+        <link rel="stylesheet" href="{{ asset('css/app.min.css') }}">
+        @else
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        @endif
 
         <!-- Scripts -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
+        @if (env('APP_ENV') == 'production')
+        <script src="{{ asset('js/app.min.js') }}" defer></script>
+        @else
         <script src="{{ asset('js/app.js') }}" defer></script>
+        @endif
     </head>
     <body class="font-sans antialiased">
         @inertia
