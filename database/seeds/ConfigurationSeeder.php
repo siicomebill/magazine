b67ec4cb68125d44460c838557cdad582fb6968e
Mattia Sinisi <mattia@snisni.it>
2020-09-29 11:34:07 +0200

    Added configuration seeder

---
+++ database/seeds/ConfigurationSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Configuration;
+use Illuminate\Database\Seeder;
+
+class ConfigurationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Configuration::create([
+            "name" => "banner",
+            "content" => []
+        ]);
+    }
+}
