f70be7514187424e0279152822c17e74830e9261
Mattia Sinisi <mattia@snisni.it>
2020-09-29 11:58:33 +0200

    Get configuration as shared data between pages

--- app/Repositories/ConfigurationRepository.php
+++ app/Repositories/ConfigurationRepository.php
@@ -7,4 +7,16 @@ use App\Models\Configuration;
 class ConfigurationRepository extends ResourceRepository
 {
     protected $model = Configuration::class;
+
+    public function get()
+    {
+        $models = ($this->model::all());
+        $result = [];
+
+        foreach ($models as $value) {
+            $result[$value->name] = $value->content;
+        }
+
+        return $result;
+    }
 }
