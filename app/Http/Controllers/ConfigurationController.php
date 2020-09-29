647df6ea5cfcc264b44b62593148f086a3823e7e
Mattia Sinisi <mattia@snisni.it>
2020-09-29 12:24:57 +0200

    Removed unused content

--- app/Http/Controllers/ConfigurationController.php
+++ app/Http/Controllers/ConfigurationController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Base\ResourceController;
-use App\Models\Configuration;
 use App\Repositories\ConfigurationRepository;
-use Illuminate\Http\Request;
 
 class ConfigurationController extends ResourceController
 {
@@ -20,80 +18,4 @@ class ConfigurationController extends ResourceController
     {
         parent::__construct($configuration);
     }
-
-    // /**
-    //  * Show the form for creating a new resource.
-    //  *
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function create()
-    // {
-    //     //
-    // }
-
-    // /**
-    //  * Store a newly created resource in storage.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function store(Request $request)
-    // {
-    //     $data = $request->all();
-    //     $config = Configuration::find($data["name"]);
-
-    //     if($config){
-    //         $config->update($data);
-    //     }
-    //     else {
-    //         $config->save($data);
-    //     }
-
-    //     redirect()->back();
-    // }
-
-    // /**
-    //  * Display the specified resource.
-    //  *
-    //  * @param  \App\Models\Configuration  $configuration
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function show(Configuration $configuration)
-    // {
-    //     //
-    // }
-
-    // /**
-    //  * Show the form for editing the specified resource.
-    //  *
-    //  * @param  \App\Models\Configuration  $configuration
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function edit(Configuration $configuration)
-    // {
-    //     //
-    // }
-
-    // /**
-    //  * Update the specified resource in storage.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @param  \App\Models\Configuration  $configuration
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function update(Request $request, Configuration $configuration)
-    // {
-    //     //
-    // }
-
-    // /**
-    //  * Remove the specified resource from storage.
-    //  *
-    //  * @param  \App\Models\Configuration  $configuration
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function destroy(Configuration $configuration)
-    // {
-    //     //
-    // }
 }
