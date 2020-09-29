d133f3913c28bd24669d8f51ff0d163f681901fa
Mattia Sinisi <mattia@snisni.it>
2020-09-29 11:05:06 +0200

    Use model's primary key to store resource

--- app/Http/Controllers/Base/ResourceController.php
+++ app/Http/Controllers/Base/ResourceController.php
@@ -81,7 +81,8 @@ abstract class ResourceController extends Controller
      */
     public function newItemPage(Request $request)
     {
-        $item = $this->resource->asModel()->find($request->id);
+        $model = $this->resource->asModel();
+        $item = $model->find($request[$model->primaryKey] ?? null);
 
         return $this->renderer::render($this->pageComponents["newItemPage"], [
             "stored" => $item ?? null,
@@ -96,7 +97,7 @@ abstract class ResourceController extends Controller
      */
     public function store(Request $request)
     {
-        return $this->resource->store($request) ? redirect()->route($this->routeNamePrefix . '.' . $this->actionRoutes["list"]) : redirect()->back('500');
+        return $this->resource->store($request) ? redirect()->route($this->routeNamePrefix . '.' . $this->actionRoutes["list"]) : redirect()->back(500);
     }
 
     /**
