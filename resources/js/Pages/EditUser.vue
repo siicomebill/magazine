9dcac845f0fc447bc059e433343ddf3d3a1dc774
Mattia Sinisi <mattia@snisni.it>
2020-09-30 16:44:33 +0200

    Editor demo

--- resources/js/Pages/EditUser.vue
+++ resources/js/Pages/EditUser.vue
@@ -1,43 +1,63 @@
 <template>
   <div>
     <div class="class container mx-auto my-4 rounded-lg p-4">
-      <form @submit.prevent="publish">
-        <div class="mb-4">
-          <input
-            type="text"
-            v-model="edited.roles"
-            placeholder="roles"
-            class="appearance-none bg-white shadow w-full leading-tight p-4 rounded-lg text-3xl font-bold text-gray-700"
-            :class="{ 'border border-red-500': error('roles') }"
-          />
-          <span class="text-red-500 text-sm italic" v-if="error('roles')">{{error('roles')}}</span>
-        </div>
-
-        <button
-          type="submit"
-          class="text-lg bg-blue-500 text-white font-bold rounded-lg mt-4 w-full p-4"
-        >Save</button>
-      </form>
+      <json-editor ref="JsonEditor" :schema="schema" v-model="model" input-wrapping-class="appearance-none bg-white shadow my-4 leading-tight p-4 rounded-lg text-3xl font-bold text-gray-700">
+        <button @click="publish" class="text-lg bg-blue-500 text-white font-bold rounded-lg mt-4 w-full p-4">Save</button>
+      </json-editor>
     </div>
   </div>
 </template>
 
 <script>
-import Layout from '@/Layouts/AppLayout'
-import Form from '~/Base/Form'
+import Layout from "@/Layouts/AppLayout";
+import Form from "~/Base/Form";
+import JsonEditor from "vue-json-ui-editor";
+
+const SCHEMA = {
+  type: "object",
+  title: "vue-json-editor demo",
+  properties: {
+    name: {
+      type: "string",
+    },
+    email: {
+      type: "string",
+    },
+  },
+};
 
 export default {
   layout: Layout,
   extends: Form,
+  components: {
+    JsonEditor,
+  },
   props: {
     roles: Array,
   },
   data() {
     return {
-      edited: {
-        roles: []
+      model: {
+        name: "Yourtion",
       },
-    }
-  }
-}
+      edited: {
+        roles: [],
+      },
+      schema: SCHEMA,
+      //   schema: {
+      //     type: "array",
+      //     title: "User roles",
+      //     items: {
+      //       type: "object",
+      //       properties: {
+      //         name: {
+      //           title: "Name",
+      //           type: "string",
+      //         },
+      //       },
+      //     },
+      //   },
+    };
+  },
+};
 </script>
