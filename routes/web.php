899a27cebd57f6e9203a5b9da40f58d196b284bc
Mattia Sinisi <mattia@snisni.it>
2020-10-01 15:46:23 +0200

    Re-organized articles route

--- routes/web.php
+++ routes/web.php
@@ -70,15 +70,17 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
             });
 
             Route::prefix('users')->group(function () {
-                Route::prefix('roles')->group(function () {
-                    Route::get('edit/{id?}', 'UserRolesController@editItemPage')->name('user.roles.write');
-                    Route::post('publish', 'UserRolesController@store')->name('user.roles.publish');
-                });
+                Route::prefix('{userId}')->group(function () {
+                    Route::prefix('roles')->group(function () {
+                        Route::get('edit/{id?}', 'UserRolesController@editItemPage')->name('user.roles.write');
+                        Route::post('publish', 'UserRolesController@store')->name('user.roles.publish');
+                    });
 
-                Route::prefix('articles')->group(function () {
-                    Route::get('{userId}', 'ArticleController@managerPage')->name('articles.ofUser.list');
-                    Route::get('{userId}/write/{id?}', 'ArticleController@editItemPage')->name('articles.ofUser.write');
-                    Route::get('{userId}/delete/{id?}', 'ArticleController@delete')->name('articles.ofUser.delete');
+                    Route::prefix('articles')->group(function () {
+                        Route::get('/', 'ArticleController@managerPage')->name('articles.ofUser.list');
+                        Route::get('write/{id?}', 'ArticleController@editItemPage')->name('articles.ofUser.write');
+                        Route::get('delete/{id?}', 'ArticleController@delete')->name('articles.ofUser.delete');
+                    });
                 });
 
                 Route::get('/', 'UserController@managerPage')->name('user.list');
