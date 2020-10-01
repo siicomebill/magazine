876b3f1fcd41a25761a9b01e8da854e52485265a
Mattia Sinisi <mattia@snisni.it>
2020-10-01 11:10:52 +0200

    Using custom list editor for roles

--- resources/js/Pages/EditUser.vue
+++ resources/js/Pages/EditUser.vue
@@ -1,27 +1,34 @@
 <template>
-  <div>
-    <div class="class container mx-auto my-4 rounded-lg p-4">
-      <p>List editor</p>
-    </div>
-  </div>
+	<div>
+		<div class="class container mx-auto my-4 rounded-lg p-4">
+			<p>List editor</p>
+			<ListEditor v-model="edited.roles" :default="{ name: '' }">
+				<template #item="{ item }">{{ item.name }}</template>
+			</ListEditor>
+		</div>
+	</div>
 </template>
 
 <script>
 import Layout from "@/Layouts/AppLayout";
 import Form from "~/Base/Form";
+import { ListEditor } from "vue-publisher";
 
 export default {
-  layout: Layout,
-  extends: Form,
-  props: {
-    roles: Array,
-  },
-  data() {
-    return {
-      edited: {
-        roles: [],
-      },
-    };
-  },
+	layout: Layout,
+	extends: Form,
+	components: {
+		ListEditor,
+	},
+	props: {
+		roles: Array,
+	},
+	data() {
+		return {
+			edited: {
+				roles: [],
+			},
+		};
+	},
 };
 </script>
