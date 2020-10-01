fb3ee889e9ec7c57384ed05d0c34e08f05c1048d
Mattia Sinisi <mattia@snisni.it>
2020-10-01 11:18:51 +0200

    Customized input list

--- resources/js/Pages/EditUser.vue
+++ resources/js/Pages/EditUser.vue
@@ -1,10 +1,22 @@
 <template>
 	<div>
 		<div class="class container mx-auto my-4 rounded-lg p-4">
-			<p>List editor</p>
-			<ListEditor v-model="edited.roles" :default="{ name: '' }">
-				<template #item="{ item }">{{ item.name }}</template>
-			</ListEditor>
+			<form @submit.prevent="publish">
+				<ListEditor v-model="edited.roles" :default="{ name: '' }">
+					<template #item="{ item }">
+						<span class=" text-3xl mb-4 font-bold text-center text-gray-500 shadow rounded-lg p-2 bg-white lg:w-1/2">
+							{{item.name}}
+						</span>
+					</template>
+				</ListEditor>
+
+				<button
+					type="submit"
+					class="text-lg bg-blue-500 text-white font-bold rounded-lg mt-4 w-full p-4"
+				>
+					Save
+				</button>
+			</form>
 		</div>
 	</div>
 </template>
