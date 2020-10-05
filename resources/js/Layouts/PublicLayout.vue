1b85309ee446fc12fc6a3a7b934cc512cddee821
Mattia Sinisi <mattia@snisni.it>
2020-10-05 17:31:02 +0200

    Added social links in footer

--- resources/js/Layouts/PublicLayout.vue
+++ resources/js/Layouts/PublicLayout.vue
@@ -50,9 +50,13 @@
 
     <section class="py-6 bg-gray-200 text-gray-700">
       <div class="container mx-auto px-4 md:px-0">
-        <p>
-          Sii Come Bill Magazine · Questa è solo una prova... infatti la grafica
-          è da rivedere
+        <div class="my-5 mx-auto text-center">
+          <a href="https://www.facebook.com/siicomebill/" target="_blank" rel="noopener" class="cta-royal mx-5">Instagram</a>
+          <a href="https://www.instagram.com/siicomebill/" target="_blank" rel="noopener" class="cta-blue mx-5">Facebook</a>
+        </div>
+
+        <p class="my-10 text-center">
+          {{$page.app.name}} · DEMO · Complete version coming soon!
         </p>
       </div>
     </section>
