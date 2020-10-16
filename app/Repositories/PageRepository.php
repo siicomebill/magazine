8bf6b778d3f599f1bee297c57529f5c6b3c79665
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-16 11:18:10 +0200

    Separated logic for page repository

--- app/Repositories/PageRepository.php
+++ app/Repositories/PageRepository.php
@@ -3,7 +3,27 @@
 namespace App\Repositories;
 
 use App\Models\Page;
+use Illuminate\Support\Arr;
 
 class PageRepository extends ConfigurationRepository {
     protected $model = Page::class;
+
+    public function get(array $elements = [])
+    {
+        $query = $this->model::whereNotNull('content')->orWhereNotNull('details');
+
+        if($elements)
+            $query = $query->whereIn('name', $elements);
+
+        $models = $query->get();
+
+        $result = [];
+
+        foreach ($models as $value) {
+            $data = $value->toArray();
+            $result[$data['name']] = Arr::except($data, 'name');
+        }
+
+        return $result;
+    }
 }
--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,16 +1,39 @@
 <template>
-  <p>About page</p>
+  <div>
+      <div class="bg-black text-white xl:py-64 py-20">
+          <div class="container mx-auto">
+              <div>
+                  <h1 class="my-auto font-banner capitalize text-6xl">{{page.name}}</h1>
+              </div>
+          </div>
+      </div>
+
+      <div class="container mx-auto my-6">
+          <Reader v-model="page.content" class="article content"/>
+      </div>
+  </div>
 </template>
 
 <script>
+import Layout from "@/Layouts/PublicLayout"
+import { Reader } from "vue-publisher"
+
 export default {
+    layout: Layout,
     props: {
         content: {}
     },
+    components: {
+        Reader,
+    },
     computed: {
         page(){
-            return this.content.about
-        }
+            return {
+                name: "about",
+                content: this.content.about.content,
+                details: this.content.about.details,
+            }
+        },
     }
 }
 </script>
