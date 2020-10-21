b98a97e33c49abbfd58fa0545c24bac7b2b5d468
sinisimattia <mattia.sinisi@virgilio.it>
2020-10-21 17:01:34 +0200

    Created generic manager page template

--- resources/js/Pages/PageManager.vue
+++ resources/js/Pages/PageManager.vue
@@ -1,63 +1,38 @@
 <template>
-  <div>
-    <div class="container mx-auto my-5">
-      <a
-        :href="$route('pages.write')"
-        class="w-full block hover:shadow-xl bg-white border-gray-200 text-center px-6 py-3 rounded-full transition-shadow duration-200"
-        >Nuovo</a
-      >
-
-      <Card
-        v-for="page in list"
-        :key="page.id"
-        :title="page.name"
-      >
-        <template #footer>
-          <div>
-            <div class="justify-end flex">
-              <a
-                :href="page.links.edit"
-                class="mx-1 hover:shadow-xl bg-gray-100 px-4 py-2 rounded-lg transition-shadow duration-200"
-              >Edit</a>
-              <a
-                :href="page.links.delete"
-                class="mx-1 hover:shadow-xl bg-red-600 text-white px-4 py-2 rounded-lg transition-shadow duration-200"
-              >Delete</a>
-            </div>
+  <ManagerPage :items="items">
+    <Card v-for="page in list" :key="page.id" :title="page.name">
+      <template #footer>
+        <div>
+          <div class="justify-end flex">
+            <a
+              :href="page.links.edit"
+              class="mx-1 hover:shadow-xl bg-gray-100 px-4 py-2 rounded-lg transition-shadow duration-200"
+              >Edit</a
+            >
+            <a
+              :href="page.links.delete"
+              class="mx-1 hover:shadow-xl bg-red-600 text-white px-4 py-2 rounded-lg transition-shadow duration-200"
+              >Delete</a
+            >
           </div>
-        </template>
-      </Card>
-
-      <div class="w-full text-center grid grid-cols-2 gap-4">
-        <a :href="previousPage" class="block bg-white px-6 py-3 rounded-full hover:shadow-xl transition-shadow duration-200">Prev</a>
-        <a :href="nextPage" class="block bg-white px-6 py-3 rounded-full hover:shadow-xl transition-shadow duration-200">Next</a>
-      </div>
-    </div>
-  </div>
+        </div>
+      </template>
+    </Card>
+  </ManagerPage>
 </template>
 
 <script>
-import Card from "~/MiniCard";
+import ManagerPage from "~/Base/ManagerPage";
 import Layout from "@/Layouts/AppLayout";
 
 export default {
   layout: Layout,
+  extends: ManagerPage,
   components: {
-    Card,
+    ManagerPage,
   },
   props: {
     items: Object,
   },
-  computed: {
-    list(){
-      return this.items.data
-    },
-    nextPage(){
-      return this.items.links.next
-    },
-    previousPage(){
-      return this.items.links.prev
-    }
-  }
 };
 </script>
