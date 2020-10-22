3e55dd1b2dbf3be2e5455615ae79b3a450d3081c
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-22 10:45:51 +0200

    Created component entity

---
+++ database/migrations/2020_10_22_084329_create_page_components_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePageComponentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_components', function (Blueprint $table) {
+            $table->foreignIdFor('App\Models\Page');
+            $table->foreignIdFor('App\Models\Component');
+
+            $table->primary(['page_id', 'component_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('page_components');
+    }
+}
