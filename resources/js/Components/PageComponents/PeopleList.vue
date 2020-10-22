c46b8dd4dd58f8e310d8dc5155a4f3a89696c33d
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-22 16:15:45 +0200

    Created paople list component

---
+++ resources/js/Components/PageComponents/PeopleList.vue
@@ -0,0 +1,28 @@
+<template>
+	<div
+		class="container mx-auto my-6 md:grid md:grid-cols-3 lg:grid-cols-4 gap-3"
+		v-if="content"
+	>
+		<div v-for="(person, i) in content" :key="i">
+			<Card
+				:title="person.name"
+				:snippet="person.desc"
+				:author="{ name: person.role }"
+				:image="person.pic"
+			/>
+		</div>
+	</div>
+</template>
+
+<script>
+import Card from "~/Card";
+
+export default {
+    props: {
+        content: Array,
+    },
+    components: {
+        Card,
+    }
+};
+</script>
