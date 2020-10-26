c7c8148966d38392a84bbb32ee435b60cf43b1d1
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-26 17:14:13 +0100

    Added proper documentation to reactable interface

--- app/Interfaces/Repositories/ReactableResourceRepositoryInterface.php
+++ app/Interfaces/Repositories/ReactableResourceRepositoryInterface.php
@@ -2,10 +2,19 @@
 
 namespace App\Interfaces\Repositories;
 
+use App\Classes\ReactionInfo;
 use Cog\Contracts\Love\Reactable\Models\Reactable;
 use Cog\Contracts\Love\Reacterable\Models\Reacterable;
 
 interface ReactableResourceRepositoryInterface extends ResourceRepositoryInterface
 {
+    /**
+     * React / Unreact to a piece of content and return reaction info.
+     * 
+     * @param Reacterable $user The entity giving the reaction
+     * @param Reactable $item The content being reacted to
+     * 
+     * @return ReactionInfo[]|array
+     */
     public function react(Reacterable $user, Reactable $item);
 }
