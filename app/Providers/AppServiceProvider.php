f90201c29ec5f231dc391614a4e6e42831be6926
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-27 10:53:07 +0100

    Removed unused content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,7 @@
 
 namespace App\Providers;
 
-use App\Repositories\ConfigurationRepository;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Support\MessageBag;
 use Illuminate\Support\ServiceProvider;
-use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(ConfigurationRepository $config)
+    public function boot()
     {
         //
     }
