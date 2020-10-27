0e6e598af856b1b14ba036b833fee436c7a1008c
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-27 17:37:05 +0100

    Added reactions setup command

---
+++ app/Console/Commands/SetupReactions.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class SetupReactions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'setup:reactions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create all the needed reaction entities';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $reactions = config('reactions', []);
+
+        foreach ($reactions as $reaction) {
+            Artisan::queue('love:reaction-type-add', [
+                '--name' => $reaction["name"],
+                '--mass' => $reaction["value"],
+            ]);
+        }
+    }
+}
