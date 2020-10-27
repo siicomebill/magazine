e1df3d17de4a1d68a4c9dea7af08b5588402c923
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-27 12:25:47 +0100

    Linted

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -6,12 +6,10 @@ use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
 
 class LoginResponse implements LoginResponseContract
 {
-
     public function toResponse($request)
-    {        
+    {
         return $request->wantsJson()
-                    ? response()->json(['two_factor' => false])
-                    : redirect()->intended(config('fortify.home'));
+            ? response()->json(['two_factor' => false])
+            : redirect()->intended(config('fortify.home'));
     }
-
 }
