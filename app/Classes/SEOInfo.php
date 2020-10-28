22e938d17fa4130ceed40bef08ca1286b4e42d8e
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-28 17:24:27 +0100

    Made properties nullable

--- app/Classes/SEOInfo.php
+++ app/Classes/SEOInfo.php
@@ -5,11 +5,11 @@ namespace App\Classes;
 class SEOInfo
 {
     public string $title;
-    public string $description;
-    public string $author;
-    public string $image;
+    public ?string $description;
+    public ?string $author;
+    public ?string $image;
 
-    public function __construct($title, $description, $author = '', $image = null)
+    public function __construct(string $title, ?string $description, ?string $author = null, ?string $image = null)
     {
         $this->title = $title;
         $this->description = $description;
