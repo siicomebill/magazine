02db5607a55a4d9437d75a61b51c7667514ee779
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-28 17:59:10 +0100

    Customizable separator

--- app/Helpers/SEO.php
+++ app/Helpers/SEO.php
@@ -6,11 +6,11 @@ use App\Interfaces\Models\SEOCompatibleInterface;
 use Meta;
 
 class SEO {
-    public static function set(SEOCompatibleInterface $item)
+    public static function set(SEOCompatibleInterface $item, string $separator = "·")
     {
         $c = $item->toSEOInfo();
 
-        Meta::title("$c->title · " . config('app.name'));
+        Meta::title($c->title . " $separator " . config('app.name'));
         Meta::set('description', $c->description);
         Meta::set('author', $c->author);
         Meta::set('image', $c->image);
