cd2bf9304b7fff4780957d9dcc0f92efa03d6919
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-11-04 15:14:00 +0100

    New image uploader design

--- resources/js/Pages/NewArticle.vue
+++ resources/js/Pages/NewArticle.vue
@@ -16,34 +16,6 @@
 				</div>
 
 				<div class="my-4">
-					<label
-            :class="{ 'border border-red-500': error('image') }"
-						class="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white"
-					>
-						<svg
-							class="w-8 h-8"
-							fill="currentColor"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 20 20"
-						>
-							<path
-								d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z"
-							/>
-						</svg>
-						<span class="mt-2 text-base leading-normal">Select a file</span>
-						<input
-							type="file"
-							@change="processFile"
-							placeholder="Thumbnail"
-							class="hidden"
-						/>
-					</label>
-					<span class="text-red-500 text-sm italic" v-if="error('image')">{{
-						error("image")
-					}}</span>
-				</div>
-
-				<div class="my-4">
 					<textarea
 						type="text"
 						v-model="edited.snippet"
@@ -85,6 +57,35 @@
 					/>
 					<span class="text-red-500 text-sm italic" v-if="error('content')">{{
 						error("content")
+					}}</span>
+				</div>
+
+        <div class="my-4">
+					<label
+            :class="{ 'border border-red-500': error('image') }"
+						class="w-64 flex flex-col items-center px-4 py-6 bg-white text-gray-900 rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-gray-900 hover:text-white"
+					>
+						<svg
+							class="w-8 h-8"
+							fill="currentColor"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+						>
+							<path
+								d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z"
+							/>
+						</svg>
+						<span class="mt-2 text-base leading-normal">Thumbnail</span>
+            <span v-if="edited.image" class="mt-2 text-gray-500 text-sm uppercase italic leading-normal">{{edited.image.name}}</span>
+						<input
+							type="file"
+							@change="processFile"
+							placeholder="Thumbnail"
+							class="hidden"
+						/>
+					</label>
+					<span class="text-red-500 text-sm italic" v-if="error('image')">{{
+						error("image")
 					}}</span>
 				</div>
 
