a10ce548a187195f575c146842b4853c8d9b9c60
sinisimattia <mattia@snisni.it>
2020-11-12 11:13:15 +0100

    Grid display

--- resources/js/Pages/ArticlesManager.vue
+++ resources/js/Pages/ArticlesManager.vue
@@ -1,36 +1,42 @@
 <template>
-  <ManagerPage :items="items" :newPage="$route('articles.write')">
-      <Card v-bind="article" v-for="article in list" :key="article.id">
-        <template #footer>
-          <div>
-            <div class="justify-end flex">
-              <a
-                :href="article.links.edit"
-                class="mx-1 hover:shadow-xl bg-gray-100 px-4 py-2 rounded-lg transition-shadow duration-200"
-              >Edit</a>
-              <a
-                :href="article.links.delete"
-                class="mx-1 hover:shadow-xl bg-red-600 text-white px-4 py-2 rounded-lg transition-shadow duration-200"
-              >Delete</a>
-            </div>
-          </div>
-        </template>
-      </Card>
-  </ManagerPage>
+    <ManagerPage :items="items" :newPage="$route('articles.write')">
+        <div class="md:grid grid-cols-3 lg:grid-cols-5 gap-4 ">
+            <Card v-bind="article" v-for="article in list" :key="article.id">
+                <template #footer>
+                    <div>
+                        <div class="justify-end flex">
+                            <a
+                                :href="article.links.edit"
+                                class="mx-1 hover:shadow-xl bg-gray-100 px-4 py-2 rounded-lg transition-shadow duration-200"
+                                >Edit</a
+                            >
+                            <a
+                                :href="article.links.delete"
+                                class="mx-1 hover:shadow-xl bg-red-600 text-white px-4 py-2 rounded-lg transition-shadow duration-200"
+                                >Delete</a
+                            >
+                        </div>
+                    </div>
+                </template>
+            </Card>
+        </div>
+    </ManagerPage>
 </template>
 
 <script>
 import ManagerPage from "~/Base/ManagerPage";
 import Layout from "@/Layouts/AppLayout";
+import Card from "~/Card"
 
 export default {
-  layout: Layout,
-  extends: ManagerPage,
-  components: {
-    ManagerPage,
-  },
-  props: {
-    items: Object,
-  },
+    layout: Layout,
+    extends: ManagerPage,
+    components: {
+      ManagerPage,
+      Card,
+    },
+    props: {
+        items: Object,
+    },
 };
 </script>
