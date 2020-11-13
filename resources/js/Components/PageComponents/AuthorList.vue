14a30da26ed209bf3807e4a2c289b8095a2ee20b
sinisimattia <mattia@snisni.it>
2020-11-13 19:17:52 +0100

    Created an author list component

---
+++ resources/js/Components/PageComponents/AuthorList.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="container mx-auto my-6" v-if="content">
+        <p class="font-banner text-3xl text-center">Le penne dietro il magazine</p>
+		<div class="md:grid md:grid-cols-3 lg:grid-cols-4 gap-3">
+			<div v-for="(person, i) in content" :key="i">
+				<Card
+					:title="person.name"
+					:snippet="person.description"
+					:author="{ name: person.role }"
+					:image="person.pic"
+					:href="person.link"
+					outsider
+				/>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import PageComponent from "~/Base/PageComponent";
+import Card from "~/Card";
+
+export default {
+	extends: PageComponent,
+	components: {
+		Card,
+	},
+};
+</script>
