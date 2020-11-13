e88990b8203a50ebf4ee3ec8e5ab3dc40db729a0
sinisimattia <mattia@snisni.it>
2020-11-13 19:20:00 +0100

    Hard-coded the component's title in

--- resources/js/Components/PageComponents/PeopleList.vue
+++ resources/js/Components/PageComponents/PeopleList.vue
@@ -1,27 +1,27 @@
 <template>
-	<div
-		class="container mx-auto my-6 md:grid md:grid-cols-3 lg:grid-cols-4 gap-3"
-		v-if="content"
-	>
-		<div v-for="(person, i) in content" :key="i">
-			<Card
-				:title="person.name"
-				:snippet="person.desc"
-				:author="{ name: person.role }"
-				:image="person.pic"
-			/>
+	<div class="container mx-auto my-6" v-if="content">
+		<p class="font-banner text-3xl text-center">Le menti dietro il magazine</p>
+		<div class="md:grid md:grid-cols-3 lg:grid-cols-4 gap-3">
+			<div v-for="(person, i) in content" :key="i">
+				<Card
+					:title="person.name"
+					:snippet="person.desc"
+					:author="{ name: person.role }"
+					:image="person.pic"
+				/>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import PageComponent from "~/Base/PageComponent"
+import PageComponent from "~/Base/PageComponent";
 import Card from "~/Card";
 
 export default {
 	extends: PageComponent,
-    components: {
-        Card,
-    }
+	components: {
+		Card,
+	},
 };
 </script>
