2b9c4b984d23223796f30a92f53d40699fb928da
sinisimattia <mattia@snisni.it>
2020-11-16 16:01:01 +0100

    Created a multi-level navbar

--- resources/js/Layouts/PublicLayout.vue
+++ resources/js/Layouts/PublicLayout.vue
@@ -1,82 +1,87 @@
 <template>
 	<div>
-		<nav
-			class="top-0 left-0 right-0 z-30 lg:bg-white lg:text-black bg-black text-white shadow-lg lg:px-6 lg:py-1 px-4"
-			:class="mode"
-		>
-			<div
-				class="container mx-auto flex items-center justify-between flex-wrap"
+		<div :class="mode" class="top-0 left-0 right-0 z-30">
+			<nav
+				class="lg:bg-white lg:text-black bg-black text-white shadow-lg lg:px-6 lg:py-1 px-4"
 			>
-				<div class="flex items-center flex-shrink-0 mr-6">
-					<a
-						href="/"
-						class="font-semibold text-xl tracking-tight font-banner uppercase"
-						>{{ $page.app.name }}</a
-					>
-				</div>
-
-				<div class="lg:inline-block lg:order-last flex items-end">
-					<div v-if="$page.auth">
-						<UserDropdown />
+				<div
+					class="container mx-auto flex items-center justify-between flex-wrap"
+				>
+					<div class="flex items-center flex-shrink-0 mr-6">
+						<a
+							href="/"
+							class="font-semibold text-xl tracking-tight font-banner uppercase"
+							>{{ $page.app.name }}</a
+						>
 					</div>
 
-					<Dropdown v-else mobileOnly title="Account">
-						<div class="lg:grid grid-cols-2 gap-2">
-							<a
-								:href="$route('login')"
-								class="block text-sm text-center px-4 py-2 leading-none border rounded border-black text-black hover:bg-black hover:text-white hover:border-transparent lg:mt-0"
-								>Login</a
-							>
-							<a
-								:href="$route('register')"
-								class="block text-sm text-center text-black px-4 py-2 leading-none lg:mt-0"
-								>Registrati</a
-							>
+					<div class="lg:inline-block lg:order-last flex items-end">
+						<div v-if="$page.auth">
+							<UserDropdown />
 						</div>
-					</Dropdown>
-				</div>
-			</div>
-		</nav>
 
-		<nav class="top-0 left-0 right-0 z-30 bg-black text-white shadow-lg">
-			<Dropdown title="Rubriche" mobileOnly containerClass="bg-black text-white">
-				<div class="text-sm lg:mb-0 mb-4 lg:inline-block">
-					<slot name="menu">
-						<ul v-if="$page.navbar.categories" class="lg:flex">
-							<li
-								v-for="category in $page.navbar.categories"
-								:key="category.id"
-								class="lg:mx-3 h-10 flex"
-							>
+						<Dropdown v-else mobileOnly title="Account">
+							<div class="lg:grid grid-cols-2 gap-2">
 								<a
-									class="flex flex-grow cursor-pointer my-auto"
-									:href="$route('categories.articles', category.id)"
+									:href="$route('login')"
+									class="block text-sm text-center px-4 py-2 leading-none border rounded border-black text-black hover:bg-black hover:text-white hover:border-transparent lg:mt-0"
+									>Login</a
 								>
-									<span
-										class="cursor-pointer block my-auto font-bold capitalize"
-										>{{ category.name }}</span
-									>
-								</a>
-								<div class="flex">
-									<Dropdown
-										class="text-black flex-grow my-auto"
-										v-if="category.children.length"
-									>
-										<a
-											class="text-sm block lg:flex-grow"
-											v-for="child in category.children"
-											:key="child.id"
-											:href="$route('categories.articles', child.id)"
-											>{{ child.name }}</a
-										>
-									</Dropdown>
-								</div>
-							</li>
-						</ul>
-					</slot>
+								<a
+									:href="$route('register')"
+									class="block text-sm text-center text-black px-4 py-2 leading-none lg:mt-0"
+									>Registrati</a
+								>
+							</div>
+						</Dropdown>
+					</div>
 				</div>
-			</Dropdown>
-		</nav>
+			</nav>
+
+			<nav class="bg-black text-white shadow-lg">
+				<Dropdown
+					title="Rubriche"
+					mobileOnly
+					containerClass="bg-black text-white"
+				>
+					<div class="text-sm lg:mb-0 mb-4 lg:inline-block">
+						<slot name="menu">
+							<ul v-if="$page.navbar.categories" class="lg:flex">
+								<li
+									v-for="category in $page.navbar.categories"
+									:key="category.id"
+									class="lg:mx-3 h-10 flex"
+								>
+									<a
+										class="flex flex-grow cursor-pointer my-auto"
+										:href="$route('categories.articles', category.id)"
+									>
+										<span
+											class="cursor-pointer block my-auto font-bold capitalize"
+											>{{ category.name }}</span
+										>
+									</a>
+									<div class="flex">
+										<Dropdown
+											class="text-black flex-grow my-auto"
+											v-if="category.children.length"
+										>
+											<a
+												class="text-sm block lg:flex-grow"
+												v-for="child in category.children"
+												:key="child.id"
+												:href="$route('categories.articles', child.id)"
+												>{{ child.name }}</a
+											>
+										</Dropdown>
+									</div>
+								</li>
+							</ul>
+						</slot>
+					</div>
+				</Dropdown>
+			</nav>
+		</div>
 
 		<main>
 			<slot></slot>
