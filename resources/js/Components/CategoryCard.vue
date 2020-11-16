44421f1463507cb02977b6ac7008af86ef7af3b3
sinisimattia <mattia@snisni.it>
2020-11-16 22:01:29 +0100

    Improved grid layout

--- resources/js/Components/CategoryCard.vue
+++ resources/js/Components/CategoryCard.vue
@@ -1,8 +1,11 @@
 <template>
 	<div class="container mx-auto rounded-lg text-white shadow-lg p-6 mb-5">
 		<div v-if="content.parent" class="mb-0">
-			<a :href="$route('categories.articles', content.parent.id)" class="text-primary font-banner uppercase text-lg">
-				{{content.parent.name}}
+			<a
+				:href="$route('categories.articles', content.parent.id)"
+				class="text-primary font-banner uppercase text-lg"
+			>
+				{{ content.parent.name }}
 			</a>
 		</div>
 		<a
@@ -17,25 +20,17 @@
 		</p>
 		<div>
 			<div class="block grid-flow-cols gap-4">
-				<div class="xl:grid grid-cols-3 gap-2">
+				<div class="sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-rows gap-4">
 					<Card
-						class="col-span-2"
-						v-bind="content.articles[0]"
-						:href="$route('articles.read', content.articles[0].id)"
+						v-bind="article"
+						v-for="article in content.articles"
+						:key="article.id"
+						:href="$route('articles.read', article.id)"
+						class="h-auto bg-transparent"
 					/>
-
-					<div>
-						<Card
-							v-bind="article"
-							v-for="article in content.articles.slice(1, 3)"
-							:key="article.id"
-							:href="$route('articles.read', article.id)"
-							class="h-auto bg-transparent"
-						/>
-					</div>
 				</div>
 
-				<div class="md:grid md:grid-cols-3 lg:grid-cols-5 gap-2">
+				<!-- <div class="md:grid md:grid-cols-3 lg:grid-cols-5 gap-2">
 					<Card
 						v-bind="article"
 						v-for="article in content.articles.slice(3)"
@@ -43,7 +38,7 @@
 						:href="$route('articles.read', article.id)"
 						class="h-auto bg-transparent"
 					/>
-				</div>
+				</div> -->
 			</div>
 		</div>
 
