83e2ec627c0f8b709e117927a659e55f7268c22c
sinisimattia <mattia@snisni.it>
2020-11-17 21:48:55 +0100

    Added buttons to share a given link

---
+++ resources/js/Components/ShareButtons.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="flex gap-4">
+		<a
+			v-for="site in sites"
+			:key="site.name"
+			class="block w-16 h-16 rounded-full flex items-center justify-center text-white"
+			:class="site.class"
+            :href="site.url(link)"
+            rel="nofollow"
+            target="_blank"
+		>
+			<Icon :icon="['fab', site.icon]" size="2x"/>
+		</a>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			sites: [
+				{
+					icon: "facebook",
+					class: "bg-blue-500",
+					url: (link) => {
+						return `https://www.facebook.com/sharer/sharer.php?u=${link}`
+					},
+                },
+                {
+					icon: "twitter",
+					class: "bg-blue-400",
+					url: (link) => {
+						return `https://twitter.com/share?&url=${link}&hashtags=siicomebillmagazine,siicomebill`
+					},
+                },
+                {
+					icon: "reddit",
+					class: "bg-orange-600",
+					url: (link) => {
+						return `http://www.reddit.com/submit?url=${link}`
+					},
+				},
+			],
+		};
+    },
+    props: {
+        link: {
+            type: String,
+            required: true,
+        }
+    }
+};
+</script>
