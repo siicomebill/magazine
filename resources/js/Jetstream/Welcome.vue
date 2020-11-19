888b8c1ca6f572478d6eac7e254e14abf0782db8
sinisimattia <mattia@snisni.it>
2020-11-19 14:59:05 +0100

    New button to go to writer's page

--- resources/js/Jetstream/Welcome.vue
+++ resources/js/Jetstream/Welcome.vue
@@ -174,6 +174,43 @@
 			<div class="p-6" v-if="userIsAdmin">
 				<div class="flex items-center">
 					<div class="text-lg text-gray-600 leading-7 font-semibold">
+						<a :href="$route('user.ofRole', {role: 'writer'})">Scrittori</a>
+					</div>
+				</div>
+
+				<div>
+					<div class="mt-2 text-sm text-gray-500">
+						Gestisci il tuo team di scrittori.
+					</div>
+
+					<a :href="$route('user.ofRole', {role: 'writer'})">
+						<div
+							class="mt-3 flex items-center text-sm font-semibold text-blue-500"
+						>
+							<div>Vai</div>
+
+							<div class="ml-1 text-blue-500">
+								<svg
+									viewBox="0 0 20 20"
+									fill="currentColor"
+									class="arrow-right w-4 h-4"
+								>
+									<path
+										fill-rule="evenodd"
+										d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+										clip-rule="evenodd"
+									/>
+								</svg>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+
+
+			<div class="p-6" v-if="userIsAdmin">
+				<div class="flex items-center">
+					<div class="text-lg text-gray-600 leading-7 font-semibold">
 						<a :href="$route('sponsors.list')">Sponsors</a>
 					</div>
 				</div>
