851def2b7c47f761af04bf72eb85e1c6eee32175
sinisimattia <mattia@snisni.it>
2020-12-12 16:01:11 +0100

    Simplified card components by using mixins

--- resources/js/Components/Card.vue
+++ resources/js/Components/Card.vue
@@ -36,16 +36,16 @@
 
 <script>
 import Thumbnail from "~/Thumbnail";
+import ContentResource from "@/Mixins/ContentResource";
 
 export default {
+	mixins: [
+		ContentResource,
+	],
 	components: {
 		Thumbnail,
 	},
 	props: {
-		id: {
-			type: [Number, String],
-			required: false,
-		},
 		title: {
 			type: String,
 			required: true,
@@ -59,22 +59,6 @@ export default {
 			type: Object,
 			required: false,
 		},
-		href: {
-			type: String,
-			required: false,
-		},
-		outsider: {
-			type: Boolean,
-			default: false,
-		},
 	},
-	computed: {
-		rel(){
-			return this.outsider ? 'noopener noreferrer nofollow' : ''
-		},
-		target(){
-			return this.outsider ? '_blank' : ''
-		}
-	}
 };
 </script>
