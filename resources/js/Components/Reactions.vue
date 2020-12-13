b3109037dc086e5ce2943ac6d9ac8114a8564e3f
sinisimattia <mattia@snisni.it>
2020-12-13 18:37:52 +0100

    Generalized reaction logic and separated it into a mixin

--- resources/js/Components/Reactions.vue
+++ resources/js/Components/Reactions.vue
@@ -23,53 +23,11 @@
 </template>
 
 <script>
-import axios from "axios";
+import Reactable from "@/Mixins/Reactable";
 
 export default {
-	props: {
-		givenReactions: {
-			type: Array,
-			required: true,
-		},
-		reactTo: {
-			type: String,
-			required: true,
-		},
-		itemId: {
-			type: [String, Number],
-			required: true,
-		},
-	},
-	methods: {
-		react(reactionType) {
-			//FIXME Generalize this method and export it to a mixin
-			axios
-				.post(`/react/${this.reactTo}`, {
-					item: {
-						id: this.itemId,
-					},
-					reaction: {
-						type: reactionType,
-					},
-				})
-				.then((response) => {
-					this.reactions = response.data;
-				})
-				.catch((error) => {
-					if (error.response.status == 401)
-						this.$redirect(this.$route("login"));
-				});
-		},
-	},
-	data() {
-		return {
-			reactions: this.givenReactions,
-		};
-	},
-	computed: {
-		sortedReactions() {
-			return this.reactions.sort((a, b) => (a.id > b.id ? 1 : -1));
-		},
-	},
+	mixins: [
+		Reactable,
+	],
 };
 </script>
