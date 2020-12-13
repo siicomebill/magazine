05af74f8f42f58c54bacb6d1fb56dcc7cf4b39d0
sinisimattia <mattia@snisni.it>
2020-12-13 18:34:31 +0100

    Fixed background color for reactions

--- resources/js/Components/Reactions.vue
+++ resources/js/Components/Reactions.vue
@@ -7,14 +7,14 @@
 			type="button"
 			class="block"
 		>
-			<div :class="{'grid': reaction.count > 0}" class="shadow-lg rounded-lg grid-cols-4">
+			<div :class="{'grid': reaction.count > 0}" class="shadow-lg rounded-lg grid-cols-4 bg-white">
 				<span
 					class="col-span-3 text-white flex items-center justify-items-center rounded-lg appearance-none bg-gradient-to-br from-blue-600 to-blue-700 py-2 px-4 hover:bg-gradient-to-l hover:from-white hover:to-blue-300 hover:text-blue-600 transition-all duration-300"
 				>
 					<span class="m-auto">{{ reaction.name }}</span>
 				</span>
 
-				<span v-if="reaction.count > 0" class="flex items-center justify-items-center rounded-r-lg appearance-none bg-white text-blue-600">
+				<span v-if="reaction.count > 0" class="flex items-center justify-items-center rounded-r-lg appearance-none text-blue-600">
 					<span class="m-auto">{{ reaction.count }}</span>
 				</span>
 			</div>
