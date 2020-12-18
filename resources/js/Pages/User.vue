12f6a034012977d18a211c6e53765dcbe951f704
sinisimattia <mattia@snisni.it>
2020-12-18 18:39:47 +0100

    Basic user showcase page

---
+++ resources/js/Pages/User.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="p-4">
+        <div class="flex items-center text-white">
+            <AuthorBadge v-bind="user" class="mx-auto"/>
+        </div>
+
+        <div>
+            <ArticleCard
+                v-for="article in user.articles"
+                :key="article.id"
+                v-bind="article"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+        AuthorBadge: () => import("~/AuthorBadge"),
+        ArticleCard: () => import("~/ArticleCard"),
+    },
+    props: {
+        user: Object
+    }
+}
+</script>
