3589b4db56c0826a943b1f235fe69fcd2b50d470
sinisimattia <mattia@snisni.it>
2020-12-18 18:36:03 +0100

    Using padding for spacing instead of margin in author badge component

--- resources/js/Components/AuthorBadge.vue
+++ resources/js/Components/AuthorBadge.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="my-4 rounded-lg flex flex-col md:flex-row items-center gap-4">
+	<div class="py-4 rounded-lg flex flex-col md:flex-row items-center gap-4">
 		<div
 			v-if="profile_photo_url"
 			class="inline-block border-4 border-white p-0 rounded-full"
