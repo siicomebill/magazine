44b8aaa6131e69dcc484d4f0eafb4da122dd95af
sinisimattia <mattia@snisni.it>
2021-02-13 22:51:14 +0100

    Example home view

--- resources/views/amp/home.blade.php
+++ resources/views/amp/home.blade.php
@@ -1,1 +1,10 @@
-<!-- AMP Home -->
+@extends('layouts.amp')
+
+@section('page')
+
+<h1>{{ config('app.name') }}</h1>
+<h4>Create "app" blade layout for AMP pages</h4>
+<p>Only import CSS in that one</p>
+
+
+@endsection
