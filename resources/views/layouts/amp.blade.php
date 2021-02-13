51f3261fcd3ff8544efb06561d9ac6e58548b5f9
sinisimattia <mattia@snisni.it>
2021-02-13 22:51:02 +0100

    Created AMP Blade layout

---
+++ resources/views/layouts/amp.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html ⚡ lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>
+            @yield('title', 'Home') · {{ config('app.name', 'Bill') }}
+        </title>
+
+        <link rel="canonical" href="{{ request()->canonicalUrl }}">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script async src="https://cdn.ampproject.org/v0.js"></script>
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.2.1/dist/alpine.js" defer></script>
+    </head>
+    <body>
+        <div class="font-sans text-gray-900 antialiased">
+            @yield('page')
+        </div>
+    </body>
+</html>
