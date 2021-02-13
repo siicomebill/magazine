37423b410b7cd10eb4555be5a3fe76a3e7b23ebe
sinisimattia <mattia@snisni.it>
2021-02-13 20:43:57 +0100

    Defined 'amp' as single route middleware instead of group

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,10 +46,6 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
-        'amp' => [
-            \App\Http\Middleware\AMPMiddleware::class,
-        ]
     ];
 
     /**
@@ -60,6 +56,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'amp' => \App\Http\Middleware\AMPMiddleware::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
