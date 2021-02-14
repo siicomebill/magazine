904e740cb9600aec6cb0dd05e836f638c780415b
sinisimattia <mattia@snisni.it>
2021-02-14 11:10:40 +0100

    Added link to categories

--- resources/views/amp/home.blade.php
+++ resources/views/amp/home.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.amp')
 
 @section('page')
+    <div class="p-4 text-center border-b border-gray-400 rounded-t-lg">
+        <a href="{{route('amp.categories')}}" class="font-bold text-primary">Vai alle categorie</a>
+    </div>
+
     <div class="py-4 px-6 xl:grid grid-cols-2">
         @foreach ($articles as $article)
             @component('components.card', [
@@ -10,6 +14,12 @@
                 'image' => $article->image,
                 'link' => route('amp.articles.read', ($article->slug ?? $article->id)),
             ])
+                @if (isset($article->category))
+                    @slot('header')
+                        <a href="{{ route('amp.categories.articles', $article->category->id) }}">{{ $article->category->name }}</a>
+                    @endslot
+                @endif
+
                 @slot('footer')
                     <p class="font-handwritten text-4xl">Scritto da <a class="font-bold" href="{{route('amp.user.page', $article->author->id)}}">{{$article->author->name}}</a></p>
                 @endslot
