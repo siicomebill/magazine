0c09967e2a3424f0fd54290c60f8b096ca58004c
sinisimattia <mattia@snisni.it>
2021-02-14 22:31:02 +0100

    Added missing AMP declarations

--- resources/views/layouts/amp.blade.php
+++ resources/views/layouts/amp.blade.php
@@ -19,7 +19,9 @@
 
         <!-- Scripts -->
         <script async src="https://cdn.ampproject.org/v0.js"></script>
-        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.2.1/dist/alpine.js" defer></script>
+
+        <!-- AMP Boilerplate -->
+        <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
     </head>
     <body class="font-sans antialiased bg-dark text-white banner">
         <div class="sticky top-0 py-2 bg-dark">
