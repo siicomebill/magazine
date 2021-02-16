674025d05c695d912f9042b67cb6a211ec9dc79d
sinisimattia <mattia@snisni.it>
2021-02-16 13:06:42 +0100

    Minor dropdown fixes

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ open: false }" class="inline-flex relative">
+<div x-data="{ open: false }" class="inline-flex relative items-center">
     <span @click="open = true" class="flex items-center focus:outline-none cursor-pointer">
         <span>
             @if (isset($title))
@@ -7,7 +7,7 @@
         </span>
 
         <span class="block pl-2">
-            <img src="/img/arrow-down.svg" alt class="h-4 w-4 arrow" x-bind:class="{ 'transform rotate-180': open }" />
+            <img src="/img/arrow-down.svg" alt class="h-4 w-4" x-bind:class="{ 'transform rotate-180': open }" />
         </span>
     </span>
 
@@ -15,7 +15,7 @@
         x-show="open"
         @click.away="open = false"
 
-        class="order-first flex-col absolute top-full z-40 align-items-center shadow-lg border-2 border-black py-2 px-4 rounded-lg"
+        class="order-first flex-col absolute top-full z-40 align-items-center shadow-lg bg-white text-black border-2 border-black py-2 px-4 rounded-lg"
     >
         {{$body ?? ''}}
     </div>
