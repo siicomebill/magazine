f06f8252ecd4cade8e5215b69e505c12acd5c6fd
sinisimattia <mattia@snisni.it>
2021-02-16 14:50:51 +0100

    Better layout for mobile

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,5 +1,5 @@
-<div x-data="{ open: false }" class="inline-flex relative items-center">
-    <span @click="open = true" class="flex items-center focus:outline-none cursor-pointer">
+<div x-data="{ open: false }" class="flex relative items-center">
+    <span @click="open = !open" class="flex items-center focus:outline-none cursor-pointer {{ ($mobileOnly ?? '') ? 'lg:hidden' : '' }}">
         <span>
             @if (isset($title))
                 {{$title}}
@@ -12,10 +12,10 @@
     </span>
 
     <div
-        x-show="open"
+        x-bind:class="{ {{ ($mobileOnly ?? '') ? 'hidden lg:block' : 'hidden' }}: !open }"
         @click.away="open = false"
 
-        class="order-first flex-col absolute top-full z-40 align-items-center shadow-lg bg-white text-black border-2 border-black py-2 px-4 rounded-lg"
+        class="order-first flex-col absolute right-0 top-full z-40 align-items-center shadow-lg bg-white text-black border-2 border-black rounded-lg {{($mobileOnly ?? '') ? 'lg:relative lg:border-none lg:shadow-none lg:p-0' : 'py-2 px-4'}}"
     >
         {{$body ?? ''}}
     </div>
