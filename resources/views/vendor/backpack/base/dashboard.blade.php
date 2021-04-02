63ba997f9a99692188e5d81ca76a7a1de5b59530
Mattia Sinisi <mattia@snisni.it>
2021-04-03 01:01:39 +0200

    Added disclaimer to dashboard

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -1,9 +1,5 @@
 @extends(backpack_view('blank'))
 
-<div>
-    <p>DEMO Only.</p>
-</div>
-
 @php
     $widgets['before_content'][] = [
         'type'        => 'jumbotron',
@@ -11,6 +7,14 @@
         'content'     => trans('backpack::base.use_sidebar'),
         'button_link' => backpack_url('/'),
         'button_text' => trans('Go Fullscreen'),
+    ];
+
+    $widgets['after_content'][] = [
+        'type'         => 'alert',
+        'class'        => 'alert alert-danger mb-2',
+        'heading'      => 'DEMO',
+        'content'      => 'Questa nuova dashboard è ancora in fase di testing.',
+        'close_button' => true, // show close button or not
     ];
 @endphp
 
