be975ece12ccc743a71457f8c45d35995cbf9470
Mattia Sinisi <mattia@snisni.it>
2021-04-02 13:42:48 +0200

    Published dashboard view file

---
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends(backpack_view('blank'))
+
+<div>
+    <p>DEMO Only.</p>
+</div>
+
+@php
+    $widgets['before_content'][] = [
+        'type'        => 'jumbotron',
+        'heading'     => trans('backpack::base.welcome'),
+        'content'     => trans('backpack::base.use_sidebar'),
+        'button_link' => backpack_url('logout'),
+        'button_text' => trans('backpack::base.logout'),
+    ];
+@endphp
+
+@section('content')
+@endsection
