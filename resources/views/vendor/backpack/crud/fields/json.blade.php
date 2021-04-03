4025d4898fdc43c8c925c1a096d11dd5c14077da
Mattia Sinisi <mattia@snisni.it>
2021-04-03 12:58:17 +0200

    Created working JSON editor

---
+++ resources/views/vendor/backpack/crud/fields/json.blade.php
@@ -0,0 +1,42 @@
+<!-- field_type_name -->
+@include('crud::fields.inc.wrapper_start')
+    <label>{!! $field['label'] !!}</label>
+    <div id="editor-{{ $field['name'] }}"></div>
+
+    {{-- HINT --}}
+    @if (isset($field['hint']))
+        <p class="help-block">{!! $field['hint'] !!}</p>
+    @endif
+@include('crud::fields.inc.wrapper_end')
+
+@if ($crud->fieldTypeNotLoaded($field))
+    @php
+        $crud->markFieldTypeAsLoaded($field);
+    @endphp
+
+    {{-- FIELD EXTRA CSS  --}}
+    {{-- push things in the after_styles section --}}
+    @push('crud_fields_styles')
+        <!-- no styles -->
+    @endpush
+
+    {{-- FIELD EXTRA JS --}}
+    {{-- push things in the after_scripts section --}}
+    @push('crud_fields_scripts')
+        <script src="https://unpkg.com/vue"></script>
+        <script src="https://unpkg.com/v-jsoneditor"></script>
+        
+        <script>
+            new Vue({
+                el: '#editor-{{ $field['name'] }}',
+                data: {
+                    content: {!! json_encode( isset($field['value']) ? $field['value'] : [] ) !!},
+                },
+                components: {
+                    Editor: VJsoneditor.default
+                },
+                template: "<div><textarea name='{{ $field['name'] }}' v-html='content' style='display:none'></textarea><Editor v-model='content'/></div>"
+            });
+        </script>
+    @endpush
+@endif
