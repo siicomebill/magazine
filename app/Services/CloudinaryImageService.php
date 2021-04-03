6d90d4f772107533f4062ca514f664fc32c3b37e
Mattia Sinisi <mattia@snisni.it>
2021-04-03 15:28:52 +0200

    Re-written Cloudinary Image Service

--- app/Services/CloudinaryImageService.php
+++ app/Services/CloudinaryImageService.php
@@ -5,13 +5,13 @@ namespace App\Services;
 use App\Classes\UploadResult;
 use App\Interfaces\Services\ImageServiceInterface;
 use Illuminate\Http\UploadedFile;
-use JD\Cloudder\Facades\Cloudder;
 
 class CloudinaryImageService implements ImageServiceInterface
 {
     public function upload(UploadedFile $file)
     {
-        $result = Cloudder::upload($file->path())->getResult();
-        return new UploadResult($result['url'], $result['public_id']);
+        $url = cloudinary()->upload($file->getRealPath())->getSecurePath();
+        
+        return new UploadResult($url);
     }
 }
