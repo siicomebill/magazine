a428e5f15b2785fc6106d1a6cc7350b53813ce73
Mattia Sinisi <mattia@snisni.it>
2021-04-04 20:06:02 +0200

    New icons in dashboard

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,8 +1,8 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('article') }}'><i class='nav-icon la la-question'></i> Articles</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class='nav-icon la la-question'></i> Categories</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('component') }}'><i class='nav-icon la la-question'></i> Components</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('configuration') }}'><i class='nav-icon la la-question'></i> Configurations</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('page') }}'><i class='nav-icon la la-question'></i> Pages</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('sponsor') }}'><i class='nav-icon la la-question'></i> Sponsors</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('article') }}'><i class='nav-icon la la-newspaper'></i> Articles</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class='nav-icon la la-sitemap'></i> Categories</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('component') }}'><i class='nav-icon la la-cube'></i> Components</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('configuration') }}'><i class='nav-icon la la-cogs'></i> Configurations</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('page') }}'><i class='nav-icon la la-file'></i> Pages</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('sponsor') }}'><i class='nav-icon la la-comments-dollar'></i> Sponsors</a></li>
