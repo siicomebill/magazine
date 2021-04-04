d13e69e0043be3bd37191ec623bbed4af7ef2720
Mattia Sinisi <mattia@snisni.it>
2021-04-04 22:47:35 +0200

    Added role/permission editor in dashboard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,5 +9,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind('App\Interfaces\Services\ImageServiceInterface', 'App\Services\CloudinaryImageService');
+
+        $this->app->bind(
+            \Backpack\PermissionManager\app\Http\Controllers\UserCrudController::class, //this is package controller
+            \App\Http\Controllers\Admin\UserCrudController::class //this should be your own controller
+        );
     }
 }
