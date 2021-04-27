9b5aca98fda746707a856d9ae476cd96044ba2d0
Mattia Sinisi <mattia@snisni.it>
2021-04-28 00:19:14 +0200

    Added image in dashboard

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -1,14 +1,17 @@
 @extends(backpack_view('blank'))
 
 @php
-    $widgets['before_content'][] = [
-        'type'        => 'jumbotron',
-        'heading'     => '👋 Ciao, <b>' . auth()->user()->name . '</b>!',
-        'content'     => trans('backpack::base.use_sidebar'),
-        'button_link' => backpack_url('/'),
-        'button_text' => trans('Go Fullscreen'),
+    $widgets['after_content'] = [ 
+        [
+            'type'        => 'jumbotron',
+            'heading'     => '👋 Ciao, <b>' . auth()->user()->name . '</b>!',
+            'content'     => trans('backpack::base.use_sidebar'),
+            'button_link' => backpack_url('/'),
+            'button_text' => trans('Go Fullscreen'),
+        ]
     ];
 @endphp
 
 @section('content')
+    <img src="https://i.ibb.co/zrP4qQ7/billboard.png" alt class="mx-auto" style="max-width: 500px; width: 100%">
 @endsection
